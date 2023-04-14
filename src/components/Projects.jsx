57426e1e50c66c4d8c06f131abb4c244c564227b
Adam Simonini <adam.simonini@gmail.com>
2023-04-14 17:05:22 -0400

    updated card styles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import img1 from "../assets/images/project-img1.png";
-import img2 from "../assets/images/project-img2.png";
-import img3 from "../assets/images/project-img3.png";
 import projectData from "@/projects";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 import "../styles.css";
 import ProjectCard from "./ProjectCard";
@@ -11,16 +10,18 @@ function Projects() {
   return (
     <section id="projects" className="project">
       <h1 className="text-center text-4xl font-bold py-6">Projects</h1>
-      <p className="max-w-[1000px] lg:px-6 mx-auto pr-10 pl-10">
+      <p className="max-w-[1000px] lg:px-6 mx-auto pr-10 pl-10 mb-10">
         Here is a sample of some of the projects I have worked on, either
         professionally or as a hobby. Please note that all projects are hosted
         for free on Netlify, and as such they might take a few seconds to load.
       </p>
-      <div className="grid grid-cols-3 p-10 gap-8 lg:grid-cols-1 tl:grid-cols-1 ">
-        {projectData.map((project) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
-      </div>
+      <Container>
+        <Row style={{ textAlign: "-webkit-center" }}>
+          {projectData.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </Row>
+      </Container>
     </section>
   );
 }
