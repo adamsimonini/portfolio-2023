16028da22bf4301feed0eadc458e484da0ffdb78
Adam Simonini <adam.simonini@gmail.com>
2023-04-18 12:54:53 -0400

    improved card stypes

--- src/projects.js
+++ src/projects.js
@@ -4,7 +4,7 @@ const projects = [
     name: "COVIDTrends",
     liveLink:
       "https://covid-trends-phac.netlify.app/index-en.html?HR=1,3551&mapOpen=false",
-    githubLink: "https://github.com/adamsimonini/COVIDTrends",
+    githubLink: "",
     photoName: "covid-trends",
     description:
       "COVIDTrends is a tool that provides summary data about COVID‑19 in your area. When you search by municipality (city, town), or by the first 3 characters of your postal code, data from your health region will appear.",
